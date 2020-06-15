317ecbce871ec00f71dc180279aa01c0952c01f3
Drew <drew@drewalthage.com>
2020-06-15 13:39:03 -0600

    route test example

---
+++ src/components/route/route.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class='route-example'>
+    <h5>Id - {{ routeId }}</h5>
+    <h1>Path - {{ routePath  }}</h1>
+    <div v-if="isAdmin" class="admin-wrapper">
+      admin!
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "route-example",
+  computed: {
+    routeId() {
+      return this.$route.params.id
+    },
+    routePath() {
+      return this.$route.path
+    },
+    isAdmin() {
+      return this.$route.path.includes('admin')
+    }
+  }
+};
+</script>
