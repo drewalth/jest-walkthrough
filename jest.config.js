b0c7d85491c629966a252ec0822d7fb8051e2dcf
Drew <andrew.althage@gmail.com>
2021-07-06 09:33:53 -0600

    coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-  verbose: true
+  verbose: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/main.js',
+    '!src/**/index.js',
+    '!src/**/create-wrapper.js'
+  ],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    '__mock-data__',
+    'services',
+    'src/utils',
+    'src/locales'
+  ],
+  coverageReporters: ['lcov', 'text-summary'],
+  coverageDirectory: 'coverage',
 }
